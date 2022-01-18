6fe9c1d0cf7d17f003b61908dd14eb986aa2d342
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-01-18 16:05:39 +0530

    Add files via upload
    Recursion is a process in which the function calls itself indirectly or directly in order to solve the problem. The function that performs the process of recursion is called a recursive function

---
+++ 14_recursion is used to print all possible words from phone digits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+string keypadArr[]={"","bc","def","ghi","jkl","mno"};
+
+void keypad(string s,string ans){
+	if(s.length()==0){
+		cout<<ans<<endl;
+		return;
+	}
+	char ch=s[0];
+	string code = keypadArrp[ch-'0'];
+	string ros = s.substr(1);
+	
+	for(int i=0;i<code.length();i++){
+		keypad(ros,ans + code[i]);
+	}
+}
+int main(){
+	keypad("23","");
+	return 0;
+}
